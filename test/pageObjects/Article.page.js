e036aaffeae61118ffb2081e04dc4669577b9fe1
Raul Martin <rmartin@weston-ins.com>
2022-09-13 12:10:42 +0200

    Testing Multiple Tags

--- test/pageObjects/Article.page.js
+++ test/pageObjects/Article.page.js
@@ -4,9 +4,11 @@ class Article extends Generic {
     get $container () { return $('[data-qa-id="article-page"]'); }
     get $title () { return $('[data-qa-id="article-title"]'); }
     get $body () { return $('[data-qa-id="article-body"]'); }
-    get $tags () { return $('[data-qa-id="article-tags"]'); }
+    get $tags () { return $$('[data-qa-id="article-tags"]'); }
+    get $$tags () {return $$('[data-qa-id="article-tags"] [data-qa-type="article-tag"]');}
     get $edit () { return $('[data-qa-id="article-edit"]'); }
     get $delete () { return $('[data-qa-id="article-delete"]'); }
+    get tags () { return this.$$tags.map($tag => $tag.getText()); }
 }
 
 module.exports = Article;
--- test/specs/editor.js
+++ test/specs/editor.js
@@ -20,6 +20,15 @@ describe('Post Editor', function () {
     editor.load();
     });
 
+    it('should load page properly', function () {
+        expect(browser).toHaveUrl(editor.url.href);
+        expect(editor.$title).toExist();
+        expect(editor.$description).toExist();
+        expect(editor.$body).toExist();
+        expect(editor.$tags).toExist();
+        expect(editor.$publish).toExist();
+    });
+
     it('should let you publish a new post', function () {
         const articleDetails = {
         title: chance.sentence({ words: 3 }),
@@ -27,14 +36,18 @@ describe('Post Editor', function () {
         body: chance.paragraph({ sentences: 4 }),
         tags: [chance.word(), chance.word()]
         };
+
         editor.submitArticle(articleDetails);
-        //expect(article.$title).toHaveText(articleDetails.title);
-        //expect(article.$body).toHaveText(articleDetails.body);
+
+        expect(article.$title).toHaveText(articleDetails.title);
+        expect(article.$body).toHaveText(articleDetails.body);
+        //expect(article.tags).toEqual(articleDetails.tags);
 
         const slug = articleDetails.title
             .toLowerCase()
             .replace(/ /g, '-')
             .replace(/[^\w-]+/g, '');
+
         // expect to be on new article page
         expect(browser).toHaveUrl(`articles/${slug}`, { containing: true });
 
